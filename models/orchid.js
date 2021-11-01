ef07259b34a953bcc7a3edf6012482a2ca37c2b0
MAI THINH <you@example.com>
2021-11-01 12:32:42 +0700

    thay doi url

--- models/orchid.js
+++ models/orchid.js
@@ -1,17 +1,17 @@
 class Orchid{
-    constructor(Rose_id, Height, description, humidity, 
-                intermediate, location, name, url_m, science_name, warm, note){
+    constructor(Rose_id, Appellation, Bloom_size, Habit, 
+        Height, Name, Position, Uses, Petal_count, url, note){
         this.Rose_id = Rose_id;
+        this.Appellation = Appellation;
+        this.Bloom_size = Bloom_size;
+        this.Habit = Habit;
         this.Height = Height;
-        this.description = description;
-        this.humidity = humidity;
-        this.intermediate = intermediate;
-        this.location = location;
-        this.science_name = science_name;
-        this.name = name;
-        this.url_m = url_m;
-        this.warm = warm;
-        this.note = note;
+        this.Name = Name;
+        this.Petal_count = Petal_count;
+        this.Position = Position;
+        this.Uses = Uses;
+        this.url = url;
+       // this.note = note;
     }
 }
 
