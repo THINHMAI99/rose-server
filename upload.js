b90b1c7f21ea48b423df23ccdefcfdaf2aa91bf8
MAI THINH <you@example.com>
2021-11-02 20:01:31 +0700

    ok

--- upload.js
+++ upload.js
@@ -1,3 +1,5 @@
+//load camera
+
 const admin = require('firebase-admin');
 const serviceAccount = require('./serviceAccountKey.json');
 
@@ -35,40 +37,40 @@ fs.readdir(directoryPath, function(err, files) {
   });
 });
 
+// //load local
+// const express = require('express');
+// const multer = require('multer');
+// const bodyParser = require('body-parser');
 
-const express = require('express');
-const multer = require('multer');
-const bodyParser = require('body-parser');
+// const app = express();
+// app.use(bodyParser.json());
 
-const app = express();
-app.use(bodyParser.json());
+// const storage = multer.diskStorage({
+//   destination(req, file, callback) {
+//     callback(null, './images');
+//   },
+//   filename(req, file, callback) {
+//     callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`);
+//   },
+// });
 
-const storage = multer.diskStorage({
-  destination(req, file, callback) {
-    callback(null, './images');
-  },
-  filename(req, file, callback) {
-    callback(null, `${file.fieldname}_${Date.now()}_${file.originalname}`);
-  },
-});
+// const upload = multer({ storage });
 
-const upload = multer({ storage });
+// app.get('/', (req, res) => {
+//   res.status(200).send('You can post to /api/upload.');
+// });
 
-app.get('/', (req, res) => {
-  res.status(200).send('You can post to /api/upload.');
-});
+// app.post('/api/upload', upload.array('photo', 3), (req, res) => {
+//   console.log('file', req.files);
+//   console.log('body', req.body);
+//   res.status(200).json({
+//     message: 'success!',
+//   });
+// });
 
-app.post('/api/upload', upload.array('photo', 3), (req, res) => {
-  console.log('file', req.files);
-  console.log('body', req.body);
-  res.status(200).json({
-    message: 'success!',
-  });
-});
-
-app.listen(process.env.PORT || 3000, () => {
-  console.log(
-    `server is running at http://localhost:${process.env.PORT || 3000}`
-  );
-});
+// app.listen(process.env.PORT || 3000, () => {
+//   console.log(
+//     `server is running at http://localhost:${process.env.PORT || 3000}`
+//   );
+// });
 
