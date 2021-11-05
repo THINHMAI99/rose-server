192c83486d585c338f275084bb6fc8f0e9066c7d
MAI THINH <you@example.com>
2021-11-06 00:09:19 +0700

    xoalink

--- routes/feed-routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getAllFeeds } = require('../controllers/feedController')
-const router = express.Router();
-
-router.get('/feeds', getAllFeeds);
-
-module.exports = {
-    routes: router
-}
